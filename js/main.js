71450aee4e8011f43e6e0ba40d4a87171be2ea55
oumaymajaouadi <60550313+oumaymajaouadi@users.noreply.github.com>
2020-04-25 16:38:41 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(".img1").hover(function(){
+    $(".btn1").toggleClass('btnHover')
+})
+
+$(".img2").hover(function(){
+    $(".btn2").toggleClass('btnHover')
+})
+
+$(".img3").hover(function(){
+    $(".btn3").toggleClass('btnHover')
+})
+$(".img4").hover(function(){
+    $(".btn4").toggleClass('btnHover')
+})
+
+$(".img5").hover(function(){
+    $(".btn5").toggleClass('btnHover')
+})
+
+$(".img6").hover(function(){
+    $(".btn6").toggleClass('btnHover')
+})
+$(".img7").hover(function(){
+    $(".btn7").toggleClass('btnHover')
+})
+
+$(".img8").hover(function(){
+    $(".btn8").toggleClass('btnHover')
+})
+
+
+
+
+
